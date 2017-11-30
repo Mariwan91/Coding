8aac0ac667410663bea832750c1b1e2bc2384eba
rahel fryad <rahel fryad@RAHELFRYAD>
2017-12-01 02:07:17 +0300

    text

--- FirstSolution/FirstSolution/Form1.cs
+++ FirstSolution/FirstSolution/Form1.cs
@@ -17,9 +17,14 @@ namespace FirstSolution
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+       
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
+
+       
+       
     }
 }
